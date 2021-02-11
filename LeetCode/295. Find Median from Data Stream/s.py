7d4af231c2d7804806035853e1f135cca5493bd7
alscwha2 <aron.messin@gmail.com>
2021-02-10 21:09:01 -0500

    more leetcodes

---
+++ LeetCode/295. Find Median from Data Stream/s.py
@@ -0,0 +1,32 @@
+from typing import List
+from sys import argv as argv
+from collections import deque
+
+'''
+    IT HAS TO BE SORTED!
+'''
+
+
+class MedianFinder:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+
+    def addNum(self, num: int) -> None:
+
+    def findMedian(self) -> float:
+
+        # Your MedianFinder object will be instantiated and called as such:
+        # obj = MedianFinder()
+        # obj.addNum(num)
+        # param_2 = obj.findMedian()
+
+        # Your MedianFinder object will be instantiated and called as such:
+        # obj = MedianFinder()
+        # obj.addNum(num)
+        # param_2 = obj.findMedian()
+
+        # argv[1]
+        # print(Solution())

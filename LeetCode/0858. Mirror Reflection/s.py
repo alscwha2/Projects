447d798b484b3c78008d2059a052de6d4fb207cb
alscwha2 <aron.messin@gmail.com>
2022-08-04 16:40:57 -0400

    solved #0858

--- LeetCode/0858. Mirror Reflection/s.py
+++ LeetCode/0858. Mirror Reflection/s.py
@@ -3,21 +3,23 @@ from sys import argv as argv
 
 class Solution:
 	def mirrorReflection(self, p: int, q: int) -> int:
-		def gcd(a,b):
+
+		def greatest_common_divisor(a, b):
 			if a == b:
 				return a
 			if a < b:
-				return gcd(a, b-a)
+				return greatest_common_divisor(a, b-a)
 			else:
-				return gcd(a-b, b)
+				return greatest_common_divisor(a-b, b)
 
-		SOUTH, SOUTH, WEST, EAST= 0, 1, 0, 1
+
+		SOUTH, NORTH, WEST, EAST= 0, 1, 0, 1
 		CORNER = [[None, 0], [2, 1]]
 
-		greatest_divisor = gcd(p, q)
+		greatest_divisor = greatest_common_divisor(p, q)
 		num_horizontal_bounces = p // greatest_divisor
 		latitude = num_horizontal_bounces % 2
-		longitude = (num_horizontal_bounces * q) // (2 * p)
+		longitude = ((num_horizontal_bounces * q) // p) % 2
 
 
 		return CORNER[longitude][latitude]

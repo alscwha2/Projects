d6b6a07755f9cc97a659a6fef011a442c2e8f437
alscwha2 <aron.messin@gmail.com>
2022-08-04 01:34:42 -0400

    testing new script

--- LeetCode/requests/req.py
+++ LeetCode/requests/req.py
@@ -3,61 +3,31 @@ from selenium.webdriver import Chrome
 from selenium.webdriver import ChromeOptions
 from selenium.webdriver.support.wait import WebDriverWait
 from bs4 import BeautifulSoup
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
+from sys import argv
+from os import mkdir
+
+# url = argv[1]
 
 options = ChromeOptions()
 options.headless = True
 
-driver = Chrome(executable_path='/home/aaron/Downloads/chromedriver', options=options)
-driver.get("https://leetcode.com/problems/add-two-numbers/")
+while True:
+	try:
+		driver = Chrome(executable_path='/home/aaron/Downloads/chromedriver', options=options)
+		driver.get("https://leetcode.com/problems/median-of-two-sorted-arrays/")
+		wait = WebDriverWait(driver, 20)
+		title = driver.find_element_by_class_name("css-v3d350").text
+		description = driver.find_element_by_class_name("content__u3I1").get_attribute('innerHTML')
+		break
+	except NoSuchElementException as e:
+		print(e)
 
-# element = driver.find_element_by_class_name("css-v3d350")
-# print(element.text)
-
-
-# html_string = requests.get("https://leetcode.com/problems/add-two-numbers/").text
-
-wait = WebDriverWait(driver, 10)
-# wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "css-v3d350")))
-element = driver.find_element_by_class_name("css-v3d350")
-print(element.text)
-doc = BeautifulSoup(driver.page_source, "html.parser")
-# print(doc)
-
-#<div data-cy="question-title" class="css-v3d350">2. Add Two Numbers</div>
-'''
-<div class="content__u3I1 question-content__JfgR"><div><p>You are given two <strong>non-empty</strong> linked lists representing two non-negative integers. The digits are stored in <strong>reverse order</strong>, and each of their nodes contains a single digit. Add the two numbers and return the sum&nbsp;as a linked list.</p>
-
-<p>You may assume the two numbers do not contain any leading zero, except the number 0 itself.</p>
-
-<p>&nbsp;</p>
-<p><strong>Example 1:</strong></p>
-<img alt="" src="https://assets.leetcode.com/uploads/2020/10/02/addtwonumber1.jpg" style="width: 483px; height: 342px;">
-<pre><strong>Input:</strong> l1 = [2,4,3], l2 = [5,6,4]
-<strong>Output:</strong> [7,0,8]
-<strong>Explanation:</strong> 342 + 465 = 807.
-</pre>
-
-<p><strong>Example 2:</strong></p>
-
-<pre><strong>Input:</strong> l1 = [0], l2 = [0]
-<strong>Output:</strong> [0]
-</pre>
-
-<p><strong>Example 3:</strong></p>
-
-<pre><strong>Input:</strong> l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
-<strong>Output:</strong> [8,9,9,9,0,0,0,1]
-</pre>
-
-<p>&nbsp;</p>
-<p><strong>Constraints:</strong></p>
-
-<ul>
-	<li>The number of nodes in each linked list is in the range <code>[1, 100]</code>.</li>
-	<li><code>0 &lt;= Node.val &lt;= 9</code></li>
-	<li>It is guaranteed that the list represents a number that does not have leading zeros.</li>
-</ul>
-</div></div>
-'''
+title = title.split(".")
+title = f"{int(title[0]):04d}.{title[1]}"
+full_path = f"/home/aaron/Projects/LeetCode/{title}"
+mkdir(full_path)
+with open(f"{full_path}/s.py", mode='w') as f:
+	print('from typing import List\nfrom sys import argv as argv\n\n\n\n# argv[1]\n# print(Solution())', file=f)
+with open(f"{full_path}/README.md", mode='w') as f:
+	print(description, file=f)

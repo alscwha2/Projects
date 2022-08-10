1d1520ffb40be39b6c6e3e9c28c9e82def846e95
alscwha2 <aron.messin@gmail.com>
2022-08-09 22:50:18 -0400

    Finishing touches on 0005

--- LeetCode/0005. Longest Palindromic Substring/attempt2.py
+++ LeetCode/0005. Longest Palindromic Substring/attempt2.py
@@ -1,4 +1,4 @@
-'''
+"""
 Solution:
 for char in string:
     treat char as center of palindrome
@@ -6,8 +6,9 @@ for char in string:
 
 Optimization: if you have a long string of the same letter in the center of the
 palindrome this code still tests each one. Ideally you would skip them. You can
-do that with a while loop but it wouldn't be as clean
-'''
+do that with a while loop, but it wouldn't be as clean
+"""
+
 
 class Solution:
     def longestPalindrome(self, s: str) -> str:
@@ -17,12 +18,13 @@ class Solution:
             while center_end + 1 < len(s) and s[center_end + 1] == char:
                 center_end += 1
 
-            while center - 1 >= 0 and center_end + 1 < len(s)and s[center - 1] == s[center_end + 1]:
+            while center - 1 >= 0 and center_end + 1 < len(s) \
+                    and s[center - 1] == s[center_end + 1]:
                 center -= 1
                 center_end += 1
             if center_end - center > longest_indices[1] - longest_indices[0]:
                 longest_indices = (center, center_end)
-        return s[longest_indices[0]:longest_indices[1]+1]
+        return s[longest_indices[0]:longest_indices[1] + 1]
 
 
 print(Solution().longestPalindrome("aaaaaasdfghjklkjhgfdsa"))
--- LeetCode/0005. Longest Palindromic Substring/attempt3.py
+++ LeetCode/0005. Longest Palindromic Substring/attempt3.py
@@ -1,6 +1,7 @@
-'''
+"""
 Now rewritten with a while loop
-'''
+"""
+
 
 class Solution:
     def longestPalindrome(self, s: str) -> str:
@@ -13,12 +14,14 @@ class Solution:
                 center_end += 1
 
             beginning, end = center, center_end
-            while beginning - 1 >= 0 and end + 1 < len(s) and s[beginning - 1] == s[end + 1]:
+            while beginning - 1 >= 0 \
+                    and end + 1 < len(s) \
+                    and s[beginning - 1] == s[end + 1]:
                 beginning, end = beginning - 1, end + 1
             if end - beginning > longest_indices[1] - longest_indices[0]:
                 longest_indices = (beginning, end)
             center = center_end + 1
-        return s[longest_indices[0]:longest_indices[1]+1]
+        return s[longest_indices[0]:longest_indices[1] + 1]
 
 
 print(Solution().longestPalindrome("aaaaaasdfghjklkjhgfdsa"))

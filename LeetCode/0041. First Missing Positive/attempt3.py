b25501ef52f0f695e0e674ff3dffb107b30f87e4
alscwha2 <aron.messin@gmail.com>
2022-09-14 18:36:34 -0400

    copied my post from Leetcode into the docstring

--- LeetCode/0041. First Missing Positive/attempt3.py
+++ LeetCode/0041. First Missing Positive/attempt3.py
@@ -1,7 +1,17 @@
 from typing import List
 
 """
-    I think that this is the cleanest code yet. Procedural
+First, put all positive integers in order in the array. Each positve integer num should occupy position num-1 (because array positions start from 0). This can be done in O(N) time.
+
+Iterate through the array. When you encounter a number:
+If it is out of bounds, just skip it.
+If it is in bounds, put it in its proper position. You'll then have to take whatever number was sitting in that position and process that one as well. We accomplish this by simply swapping the two numbers and repeating the algorithm.
+If that position already has the correct number, just skip it. (if you don't youll end up in an infinite loop)
+Once all of the numbers are in order, just check to make sure that the entire array contains the numbers 1, ..., len(nums) in order.
+
+If one is missing, that is the answer.
+If none are missing, then the next positive integer is len(nums) + 1
+Python trick - the enumerate function takes a second variable, which defines where to start counting from. By default we count from zero, but enumerate(nums, 1) will start counting from 1. That is perfect for us, because 1 is supposed to be in the first slot, 2 in the second, etc. So if we start counting from 1, we can just compare the number to its position to make sure that it's correct!
 """
 
 class Solution:

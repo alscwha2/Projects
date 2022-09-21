943d28a288f94d0b4adacd4d0e98b60412d4e4ce
alscwha2 <aron.messin@gmail.com>
2022-09-21 19:07:32 -0400

    0044

--- LeetCode/0044. Wildcard Matching/s.py
+++ LeetCode/0044. Wildcard Matching/s.py
@@ -1,10 +1,20 @@
-from typing import List
-from sys import argv as argv
+from functools import cache
+
 
 class Solution:
+	@cache
 	def isMatch(self, s: str, p: str) -> bool:
-		
+		for i, (s_char, p_char) in enumerate(zip(s, p)):
+			if p_char == '*':
+				return self.isMatch(s[i:], p[i + 1:]) \
+					or self.isMatch(s[i + 1:], p[i:])
+			if s_char != p_char and p_char != '?':
+				return False
 
+		non_stars_remaining = set(p[len(s):]) - {'*'}
+		return len(s) <= len(p) and not non_stars_remaining
 
-# argv[1]
-# print(Solution())
+a = "babaaababaabababbbbbbaabaabbabababbaababbaaabbbaaab"
+b = "***bba**a*bbba**aab**b"
+ans = Solution().isMatch(a, b)
+print(ans)

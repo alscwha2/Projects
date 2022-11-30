cb17b196d1b24a2a3ac84a427675680512c52503
alscwha2 <aron.messin@gmail.com>
2022-11-30 12:22:15 -0500

    0997 improved

--- LeetCode/0997. Find the Town Judge/s.py
+++ LeetCode/0997. Find the Town Judge/s.py
@@ -3,20 +3,29 @@ from typing import List
 
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
-        candidates = {person for person in range(1, n+1)}
-        for truster, _ in trust:
-            candidates.discard(truster)
+        def people_who_do_not_trust_anyone():
+            candidates = {person for person in range(1, n+1)}
+            for truster, _ in trust:
+                candidates.discard(truster)
+
+            return candidates
+
+        def everyone_trusts(candidate):
+            left_to_trust = {person for person in range(1,n+1)}
+
+            left_to_trust.discard(candidate)
+            for truster, trusted in trust:
+                if trusted == candidate:
+                    left_to_trust.discard(truster)
+            return left_to_trust
+
+        candidates = people_who_do_not_trust_anyone()
         if len(candidates) != 1:
             return -1
 
-        left_to_trust = {person for person in range(1,n+1)}
         candidate = candidates.pop()
-        left_to_trust.discard(candidate)
-        for truster, trusted in trust:
-            if trusted == candidate:
-                left_to_trust.discard(truster)
 
-        return candidate if not left_to_trust else -1
+        return candidate if everyone_trusts(candidate) else -1
 
 
 

43213780123a374291670315c7367aca9a89698d
alscwha2 <aron.messin@gmail.com>
2023-02-15 15:49:56 -0500

    /home/aaron/Projects/LeetCode/0099. Recover Binary Search Tree completed

--- LeetCode/0099. Recover Binary Search Tree/s.py
+++ LeetCode/0099. Recover Binary Search Tree/s.py
@@ -1,6 +1,3 @@
-from typing import List
-from sys import argv as argv
-
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -8,27 +5,43 @@ from sys import argv as argv
 #         self.left = left
 #         self.right = right
 class Solution:
-    def recoverTree(self, root: TreeNode) -> None:
+    def recoverTree(self, root: Optional[TreeNode]) -> None:
         """
         Do not return anything, modify root in-place instead.
         """
-        if not root:
-        	return 
-        # find the one that's out of place
-        outofplace = []
-        def test(parent, node):
-        	if node.left:
-        		if node.val < node.left.val:
-        			outofplace.append((parent, node))
-        		test(node, node.left)
-        	if node.right:
-        		if node.val > node.right.val:
-        			outofplace.append((parent, node))
-        		test(node, node.right)
-        test(None, root)
-        
 
+        previous = None
+        first_found = False
+        second_found = False
+        first = None
+        last = None
 
+        def traverse(root):
+            nonlocal previous, first_found, first, last, second_found
 
-# argv[1]
-# print(Solution())
+            if root == None:
+                return
+            if first_found and second_found:
+                return
+
+            traverse(root.left)
+
+            if not previous:
+                previous = root
+
+            if root.val < previous.val:
+                if first_found:
+                    last = root
+                    second_found = True
+                    return
+                else:
+                    first = previous
+                    last = root
+                    first_found = True
+            previous = root
+
+            traverse(root.right)
+
+        traverse(root)
+        first.val, last.val = last.val, first.val
+

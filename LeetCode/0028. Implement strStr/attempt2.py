e5b48f33b94c2423de8882fb2764610be6f54e2a
alscwha2 <aron.messin@gmail.com>
2024-02-28 21:42:45 -0500

    made attempt2 a little bit cleaner in my opinion

--- LeetCode/0028. Implement strStr/attempt2.py
+++ LeetCode/0028. Implement strStr/attempt2.py
@@ -1,6 +1,6 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        for i in range(1 + (len(haystack) - len(needle))):
-            if needle == haystack[i : i + len(needle)]:
+        for i in range(len(haystack) - len(needle) + 1):
+            if haystack[i:i+len(needle)] == needle:
                 return i
         return -1

fb7795c4767367efad869db263bb1b3ec93d3d05
alscwha2 <aron.messin@gmail.com>
2024-06-04 00:54:26 -0400

    aaron completed

--- LeetCode/0108. Convert Sorted Array to Binary Search Tree/s.py
+++ LeetCode/0108. Convert Sorted Array to Binary Search Tree/s.py
@@ -1,5 +1,9 @@
 from typing import List
-from sys import argv as argv
+"""
+    The reason that this works is only becaue if you take a slice, even if the starting
+        index is out of range, it does not raise an error, rather it just returns 
+        an empty list
+"""
 
 # Definition for a binary tree node.
 # class TreeNode:
@@ -9,8 +13,10 @@ from sys import argv as argv
 #         self.right = right
 class Solution:
     def sortedArrayToBST(self, nums: List[int]) -> TreeNode:
-        
-
-
-# argv[1]
-# print(Solution())
+        if not nums:
+            return None
+        root_index = len(nums) // 2
+        root = TreeNode(nums[root_index])
+        root.left = self.sortedArrayToBST(nums[:root_index])
+        root.right = self.sortedArrayToBST(nums[root_index+1:])
+        return root

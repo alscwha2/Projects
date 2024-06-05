a210aa8b89dda0bb6244bab25bdecd23cd9a7700
alscwha2 <aron.messin@gmail.com>
2024-06-05 00:05:25 -0400

    aaron completed

---
+++ LeetCode/0145. Binary Tree Postorder Traversal/attempt2.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def postorderTraversal(self, root: Optional [TreeNode]) -> List[int]:
+        if not root:
+            return []
+        answer  = self.postorderTraversal(root.left)
+        answer += self.postorderTraversal(root.right)
+        answer.append(root.val)
+        return answer

e6eb2036dc14b36a284eec21761e99d35a443878
alscwha2 <aron.messin@gmail.com>
2024-06-11 19:19:13 -0400

    aaron completed

--- LeetCode/0231. Power of Two/s.py
+++ LeetCode/0231. Power of Two/s.py
@@ -2,6 +2,11 @@ from math import log2
 
 """
     You can use bitwise as well, but that is dependant on the integer encoding
+
+    There was a very interesting approach as well, saying that if you do bitwise
+        AND between a power of 2 and the next lowest number it will always equal 
+        0, and if it is not a power of 2 it will never equal zero. I have to think
+        about that more.
 """
 
 

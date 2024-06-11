26b14e87bfb22ea6b01e8d2593e9795999bba7b0
alscwha2 <aron.messin@gmail.com>
2024-06-10 22:04:20 -0400

    aaron completed

--- LeetCode/0079. Word Search/attempt2.py
+++ LeetCode/0079. Word Search/attempt2.py
@@ -1,6 +1,11 @@
 from typing import List
 from itertools import product
 
+'''
+    Did away with the set and just marked it as seen on the board itself.
+    Not an upgrade, just overcomplicates things. Better as a separate data structure.
+'''
+
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         m, n = len(board), len(board[0])
@@ -8,7 +13,7 @@ class Solution:
 
         def value(cell):
             row, column = cell
-            return board[row][column]
+            return board[row][column][0]
 
         def neighbors(cell):
             row, column = cell
@@ -23,20 +28,31 @@ class Solution:
                 other_cells.append((row, column+1))
             return other_cells
 
+        def mark_seen(cell):
+            row, column = cell
+            board[row][column] += 'X'
 
-        def dfs(cell, word_index, seen):
-            if cell in seen:
+        def is_seen(cell):
+            row, column = cell
+            return len(board[row][column]) > 1
+
+        def unmark(cell):
+            row, column = cell
+            board[row][column] = board[row][column][0]
+
+        def dfs(cell, word_index):
+            if is_seen(cell):
                 return False
 
-            seen.add(cell)
+            mark_seen(cell)
 
             answer = False
             if word_index == len(word)-1:
                 answer = value(cell) == word[-1]
             elif value(cell) == word[word_index]:
-                answer = any(dfs(neighbor, word_index+1, seen) for neighbor in neighbors(cell))
+                answer = any(dfs(neighbor, word_index+1) for neighbor in neighbors(cell))
 
-            seen.remove(cell)
+            unmark(cell)
             return answer
 
-        return any(dfs(cell, 0, set()) for cell in cells)
+        return any(dfs(cell, 0) for cell in cells)

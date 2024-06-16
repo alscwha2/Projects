46379b3076c0f6f49705207d324e2fd562aca34c
alscwha2 <aron.messin@gmail.com>
2024-06-16 02:10:01 -0400

    aaron completed

---
+++ LeetCode/0303. Range Sum Query - Immutable/s.py
@@ -0,0 +1,15 @@
+from itertools import accumulate
+
+
+class NumArray:
+
+    def __init__(self, nums: List[int]):
+        self.sums = list(accumulate(nums, initial=0))
+
+    def sumRange(self, left: int, right: int) -> int:
+        return self.sums[right+1] - self.sums[left]
+
+
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray(nums)
+# param_1 = obj.sumRange(left,right)
